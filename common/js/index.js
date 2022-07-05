60bbaa3773ff1a7cefd7b4acbf04ae0b539306ba
schatzen <gomieti@naver.com>
2022-07-05 14:24:03 +0900

    add: footer accordion js 파일에 주석 추가

--- common/js/index.js
+++ common/js/index.js
@@ -79,9 +79,8 @@ json.forEach((item) => {
   parent.append(fragment);
 });
 
-// accordion
+// footer accordion
 const acc = document.getElementsByClassName("acc");
-
 for (let i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function () {
     this.nextElementSibling.classList.toggle("active");
