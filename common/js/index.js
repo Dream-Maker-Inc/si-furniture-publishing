9c756aadc586b6a16f41a2260fc3432f0f70c8f4
schatzen <gomieti@naver.com>
2022-07-05 14:23:13 +0900

    feat: mobile 버전 footer accordion 구현

--- common/js/index.js
+++ common/js/index.js
@@ -78,3 +78,13 @@ json.forEach((item) => {
 
   parent.append(fragment);
 });
+
+// accordion
+const acc = document.getElementsByClassName("acc");
+
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.nextElementSibling.classList.toggle("active");
+    this.classList.toggle("acc_icon--active");
+  });
+}
