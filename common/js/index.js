eece80422e28d65a02679ea71c8973f29e5b1e32
schatzen <gomieti@naver.com>
2022-07-07 18:23:14 +0900

    feat: json data fetching with html

--- common/js/index.js
+++ common/js/index.js
@@ -22,62 +22,200 @@ const getPositionByDirection = (direction, element) => {
   }
 };
 
-//JSON 객체 생성
-let json = JSON.parse(JSON.stringify(productInformation));
-let fragment = document.createDocumentFragment();
+// 새 창 혹은 현재 창으로 연결 설정
+const setPopupByLink = (element, popupState, address) => {
+  element.href = address;
 
-// 노드 추가 순환
-json.forEach((item) => {
-  let parent = document.getElementById(item.imageID);
+  if (popupState == true) {
+    element.target = "_blank";
+  }
+};
 
-  item.data.forEach((it) => {
-    // 노드 생성
-    let roundPosition = document.createElement("div");
-    let innerRound = document.createElement("div");
-    let round = document.createElement("div");
-    let popupWrapper = document.createElement("div");
+// aria-hiddeen 설정
+const setAriaHidden = (element, roundPositionElement, hiddenState) => {
+  if (hiddenState == true) {
+    element.dataset.hidden = "true";
+    element.className = "popup__default popup__wrapper";
+    roundPositionElement.className = "round-position__default round-position";
+  } else {
+    element.dataset.hidden = "false";
+    element.className = "popup__wrapper";
+    roundPositionElement.className = "round-position";
+  }
+};
 
-    let a = document.createElement("a");
-    let title = document.createElement("h3");
-    let desc = document.createElement("h4");
-    let price = document.createElement("p");
+const makeSlide = (item) => {
+  // 노드 생성
+  let slideCard = document.createElement("a");
 
-    // 텍스트 노드 생성
-    const titleText = document.createTextNode(it.title);
-    const descText = document.createTextNode(it.description);
-    const priceText = document.createTextNode(it.price);
+  let imgWrapper = document.createElement("div");
+  let picFirst = document.createElement("img");
+  let picSecond = document.createElement("img");
 
-    // 텍스트 매칭
-    title.appendChild(titleText);
-    desc.appendChild(descText);
-    price.appendChild(priceText);
+  let infoWrapper = document.createElement("div");
+  let infoTextWrapper = document.createElement("h3");
+  let infoHeader = document.createElement("span");
+  let infoDesc = document.createElement("span");
+  let infoPirceWrapper = document.createElement("div");
+  let infoPirce = document.createElement("p");
 
-    a.append(title);
-    a.append(desc);
-    a.append(price);
-    a.href = it.link;
+  let cartWrapper = document.createElement("div");
+  let cartBtn = document.createElement("button");
+  let cartIcon = document.createElement("i");
+  let heartBtn = document.createElement("button");
+  let heartIcon = document.createElement("i");
 
-    roundPosition.className = "round-position";
-    round.className = "round";
-    innerRound.className = "inner-round";
-    popupWrapper.className = "popup__wrapper";
-    a.className = "popup";
+  slideCard.className = "swiper-slide item";
+  imgWrapper.className = "img-wrapper";
+  picFirst.className = "pic-first";
+  picSecond.className = "pic-second";
+  infoWrapper.className = "info-wrapper";
+  infoTextWrapper.className = "text-bold";
+  infoHeader.className = "info__header";
+  infoDesc.className = "info__desc";
+  infoPirceWrapper.className = "info__price text-bold";
 
-    popupWrapper.append(a);
-    round.append(innerRound);
-    roundPosition.append(popupWrapper);
-    roundPosition.append(round);
+  cartWrapper.className = "cart-wrapper";
+  cartBtn.className = "btn-cart";
+  cartIcon.className = "cart bi bi-basket2-fill";
+  heartBtn.className = "btn-heart";
+  heartIcon.className = "heart bi bi-heart";
 
-    roundPosition.style.top = it.axisY;
-    roundPosition.style.left = it.axisX;
+  // json data append
+  picFirst.src = item.firstPictureSrc;
+  picSecond.src = item.secondPictureSrc;
+  slideCard.href = item.link;
 
-    getPositionByDirection(it.direction, a);
+  infoHeader.append(item.title);
+  infoDesc.append(item.description);
 
-    fragment.append(roundPosition);
+  infoPirce.append(item.price);
+
+  // node fetching
+  imgWrapper.append(picFirst);
+  imgWrapper.append(picSecond);
+
+  infoTextWrapper.append(infoHeader);
+  infoTextWrapper.append(infoDesc);
+
+  infoPirceWrapper.append(infoPirce);
+
+  cartBtn.append(cartIcon);
+  heartBtn.append(heartIcon);
+  cartWrapper.append(cartBtn);
+  cartWrapper.append(heartBtn);
+
+  infoWrapper.append(infoTextWrapper);
+  infoWrapper.append(infoPirceWrapper);
+  infoWrapper.append(cartWrapper);
+
+  slideCard.append(imgWrapper);
+  slideCard.append(infoWrapper);
+
+  return slideCard;
+
+  // 텍스트 노드 생성
+  // const infoHeaderText = document.createTextNode(it.title);
+  // const infoDescText = document.createTextNode(it.description);
+  // const priceText = document.createTextNode(it.price);
+  // const discountPriceText = document.createTextNode(it.discountPrice);
+};
+
+// json data 불러오기
+const setJsonData = () => {
+  //JSON 객체 생성
+  let json = JSON.parse(JSON.stringify(productInformation));
+  let fragment = document.createDocumentFragment();
+  let fragment2 = document.createDocumentFragment();
+
+  let parentItem = document.getElementById("swiper-wrapper");
+
+  // 노드 추가 순환
+  json.forEach((item) => {
+    let parent = document.getElementById(item.imageID);
+
+    item.data.forEach((it) => {
+      const slideCard = makeSlide(it);
+
+      // 노드 생성
+      let roundPosition = document.createElement("div");
+      let innerRound = document.createElement("div");
+      let round = document.createElement("div");
+      let popupWrapper = document.createElement("div");
+
+      let a = document.createElement("a");
+      let title = document.createElement("h3");
+      let desc = document.createElement("h4");
+      let price = document.createElement("p");
+      let discountPrice = document.createElement("p");
+
+      // 텍스트 노드 생성
+      const titleText = document.createTextNode(it.title);
+      const descText = document.createTextNode(it.description);
+      const priceText = document.createTextNode(it.price);
+      const discountPriceText = document.createTextNode(it.discountPrice);
+
+      // 텍스트 매칭
+      title.appendChild(titleText);
+      desc.appendChild(descText);
+      price.appendChild(priceText);
+      discountPrice.appendChild(discountPriceText);
+
+      a.append(title);
+      a.append(desc);
+      a.append(price);
+
+      setPopupByLink(a, it.popup, it.link);
+      setAriaHidden(popupWrapper, roundPosition, it.hidden);
+
+      round.className = "round";
+      innerRound.className = "inner-round";
+      a.className = "popup";
+
+      popupWrapper.append(a);
+      round.append(innerRound);
+      roundPosition.append(popupWrapper);
+      roundPosition.append(round);
+
+      roundPosition.style.top = it.axisY;
+      roundPosition.style.left = it.axisX;
+
+      getPositionByDirection(it.direction, a);
+
+      fragment.append(roundPosition);
+      fragment2.append(slideCard);
+
+      console.log(slideCard);
+    });
+
+    parent.append(fragment);
+    parentItem.append(fragment2);
+    //parentItem.append(childItem);
   });
+};
 
-  parent.append(fragment);
-});
+(function () {
+  setJsonData();
+
+  const photoItems = document.querySelectorAll(".photo");
+
+  photoItems.forEach(function (photo) {
+    const popupWrapperItem = photo.querySelector(".popup__default");
+    const roundPositionItem = photo.querySelector(".round-position__default");
+
+    photo.addEventListener("mouseout", function (event) {
+      popupWrapperItem.dataset.hidden = "true";
+    });
+
+    photo.addEventListener("mouseover", function (event) {
+      popupWrapperItem.dataset.hidden = "false";
+    });
+
+    roundPositionItem.addEventListener("mouseenter", function (event) {
+      popupWrapperItem.dataset.hidden = "true";
+    });
+  });
+})();
 
 // footer accordion
 const acc = document.getElementsByClassName("acc");
@@ -87,3 +225,12 @@ for (let i = 0; i < acc.length; i++) {
     this.classList.toggle("acc_icon--active");
   });
 }
+
+// swiper slide DOM 생성 함수
+const createSwiperSlide = ({ name }) => {
+  const swiperSlide = document.createElement("div");
+  swiperSlide.className = "swiper-slide";
+  swiperSlide.innerHTML = name;
+
+  return swiperSlide;
+};
